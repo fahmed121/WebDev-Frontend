7aa1036fdf402fa2686dde41b502458cec370dc8
fahmed121 <farazahmed1323@gmail.com>
2025-04-24 19:45:58 +0100

    File Structure
    made the recommended file structure and also set up a jsx file for navigation

---
+++ SupremeGrillFrontend/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+
+function Nav(){
+    return (
+        <>
+        <nav>
+            <a href ='#'>Menu</a>
+            <a href ='#'>Basket</a>
+            <a href ='#'>Profile</a>
+            <a href ='#'>AboutUs</a>
+        </nav>
+        </>
+    )
+}
+
+export default Nav;
