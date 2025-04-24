fb5c570089d198e3826ede0b8e3d05e9cbed3c17
fahmed121 <farazahmed1323@gmail.com>
2025-04-24 23:36:20 +0100

    Bootstrap Nav bar inialised
    removed the default css and installed bootstrap for navbar so far

--- SupremeGrillFrontend/src/components/Nav.jsx
+++ SupremeGrillFrontend/src/components/Nav.jsx
@@ -2,11 +2,13 @@
 function Nav(){
     return (
         <>
-        <nav>
-            <a href ='#'>Menu</a>
-            <a href ='#'>Basket</a>
-            <a href ='#'>Profile</a>
-            <a href ='#'>AboutUs</a>
+        <nav className ="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className = "navbar-nav">
+            <a className ='nav-link' href ='#'>Menu</a>
+            <a className ='nav-link' href ='#'>Basket</a>
+            <a className ='nav-link' href ='#'>Profile</a>
+            <a className ='nav-link' href ='#'>AboutUs</a>
+            </div>
         </nav>
         </>
     )
